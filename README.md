3e5972b38bdba76bf9e504749c5eaa6224589491
Tarek <tarekmabdallah91@gmail.com>
2019-07-24 16:17:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CV App 
+simple app to add easily to my apps to show my CV
---
+++ app/src/main/java/com/gmail/tarekmabdallah91/cvapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.gmail.tarekmabdallah91.cvapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import static aboutMe.AboutMeActivity.openAboutMeActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView showCV = findViewById(R.id.show_cv_tv);
+        showCV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAboutMeActivity(v.getContext());
+            }
+        });
+    }
+}
